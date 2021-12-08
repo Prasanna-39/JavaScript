47776f7cdc16490ea04a732fb2ae61b998fe521f
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2021-12-08 14:16:11 +0530

    Add files via upload

---
+++ 2.obj.js
@@ -0,0 +1,19 @@
+let person ={
+     name:"Prasanna",
+     age:21,
+     Dob:"21.12.21",
+     bool:true,
+     silibling :{
+         brother:"nick",
+         siiter:"nicky"
+
+     }
+
+
+}
+// dot notation 
+console.log(person.name)
+console.log(person.age)
+
+//bracket notation 
+console.log(person['silibling'])
