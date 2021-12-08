04e912b8a7cb81f5d2c17db2a3c24444bc5a8abc
Prasanna-39 <74823833+Prasanna-39@users.noreply.github.com>
2021-12-08 13:43:34 +0530

    Add files via upload

---
+++ 1.index.js
@@ -0,0 +1,8 @@
+// var is old variable declaration
+//let is the updated vserion of --it can able to change the variable value ex:
+//name = "Prasa" name="Prasanna" console.log(name)----> o/p : Prasanna
+//const is a permenant var declaratio like tuple in python 
+// it can not able to change the variable value 
+let name ="Prasanna"
+const Dob="21.12.21"
+console.log("I am "+name+", i bron in "+Dob+".i am learning Javascript")
